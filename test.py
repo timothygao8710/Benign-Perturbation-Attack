5b165cdfef1eb732882291796f6f7dfc0c87d191
Benjamin Bolte <ben@bolte.cc>
2024-06-07 17:33:47 +0000

    initial_commit

---
+++ test.py
@@ -0,0 +1,13 @@
+from vllm import LLM, SamplingParams
+
+prompts = [
+    "Hello, my name is",
+    "The president of the United States is",
+    "The capital of France is",
+    "The future of AI is",
+]
+
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+
+llm = LLM(model="meta-llama/Meta-Llama-3-70B-Instruct")
+
