3bf82309b29621dfcb27508123e574399284de64
Timothy Gao <timothygao8710@gmail.com>
2024-06-18 18:47:10 -0700

    updated result from runs (this took forever ;-;)

--- peturb_stats.py
+++ peturb_stats.py
@@ -15,7 +15,7 @@ n_samples = 200 # how many samples to test, for each question
 
 res_mp = {}
 
-percentage_bins = [0.01, 0.02, 0.03, 0.05, 0.07, 0.1, 0.12, 0.15, 0.2, 0.3, 0.5, 0.75, 1]
+percentage_bins = [0.75, 1]
 
 for percent in percentage_bins:
 
